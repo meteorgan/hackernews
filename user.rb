849a1311c05f91e1204fa4748f548425757716bc
meteorgan <meteorite.gan@gmail.com>
2013-07-13 14:28:09 +0800

    fix submissions list bug

--- user.rb
+++ user.rb
@@ -4,12 +4,29 @@ require "open-uri"
 require 'date'
 
 class UserInfo
-	attr_reader :user_id, :create_date, :karma, :avg, :submission_ids, :comments_id
+	attr_reader :user_id, :create_date, :karma, :avg, :about
 
 	def initialize(user_id)
 		@user_id = user_id
+		get_user_info
 	end
 
+	def get_submissions	
+		submissions_url = "https://news.ycombinator.com/submitted?id=" + @user_id
+		result = request(submissions_url)
+		
+		regexp = %r{ago  \| <a href="item\?id=(\d+)}
+	    @submissions_ids = result.scan(regexp).flatten(1)
+	end
+
+	def get_comments
+		comments_url = "https://news.ycombinator.com/threads?id=" + @user_id
+		result = request(comments_url)
+
+		@comments_ids = result.scan(%r{user\?id=#{@user_id}.*?item\?id=(\d+)}).flatten(1)
+	end
+
+	private
 	def get_user_info
 		user_url = "https://news.ycombinator.com/user?id=" + @user_id
 		regexps = {"created"=> %r{created:</td><td>(\d+) days ago},
@@ -24,24 +41,9 @@ class UserInfo
 		@avg = user_info.scan(regexps["avg"])[0][0]
 		@about = user_info.scan(regexps["about"])[0][0]
 
-		@create_date = Date.today - created.to_i
+		@create_date = (Date.today - created.to_i).to_s
 	end
 
-	def get_submissions	
-		submissons_url = "https://news.ycombinator.com/submitted?id=" + @user_id
-		result = request(submissions_url)
-		
-	     @submissions_ids = result.match(%r{item\?id=(\d+)})[1..-1]	
-	end
-
-	def get_comments
-		comments_url = "https://news.ycombinator.com/threads?id=" + @user_id
-		result = request(submissions_url)
-
-		@comments_ids = result.match(%r{user\?id=#{@user_id}.*?item\?id=(\d+)})[1..-1]
-	end
-
-	private
 	def request(url)
 		result = ''
 		open(url) {|f|
